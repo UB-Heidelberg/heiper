63bc5c45974c4476f5d04b23bff03b9f68e558a4
Konstantin Baierer <unixprog@gmail.com>
2017-10-23 14:31:52 +0200

    :fire: Expect an array of JSON objects

--- src/server.js
+++ src/server.js
@@ -7,31 +7,40 @@ const config = require('./config')
 function createServer() {
     const app = express()
     app.use(bodyParser.json())
+    app.use(require('morgan')('dev'))
 
     Object.keys(config).forEach(pluginName => {
         Object.keys(config[pluginName]).forEach(profile => {
             console.log(`Setting up /${pluginName}/${profile}`)
             const plugin = new(require(`./plugin/${pluginName}`))(config[pluginName][profile])
 
-            app.post(`/${pluginName}/${profile}`, (req, res, next) => {
-                const input = req.body
-                const opts = {
-                    useSandbox: req.query.sandbox && req.query.sandbox.match(/true|1/),
-                    dryRun: req.query.dryRun && req.query.dryRun.match(/true|1/),
-                }
-                const invalid = validate(input)
-                if (invalid) return res.status(400).send({message: 'Validation failed', errors: invalid})
-                else if (opts.dryRun) return res.status(200).send({valid:true})
+          app.post(`/${pluginName}/${profile}`, (req, res, next) => {
+            const opts = {
+              useSandbox: req.query.sandbox && req.query.sandbox.match(/true|1/),
+              dryRun: req.query.dryRun && req.query.dryRun.match(/true|1/),
+            }
+            if (!Array.isArray(req.body)) {
+              return res.status(400).send({error: "Must pass an array of JSON objects"})
+            }
+            Promise.all(req.body.map(input => {
+              const invalid = validate(input)
+              if (invalid) return Promise.reject({
+                status: 415,
+                data: {message: 'Validation failed', errors: invalid}
+              })
+              else if (opts.dryRun) return Promise.resolve({
+                status: 200,
+                data: {valid: ! invalid}
+              })
+              return new Promise((resolve, reject) => {
                 plugin.registerDOI(input, opts)
-                    .then(({status, data}) => {
-                        res.status(status).send({status, data})
-                    })
-                    .catch(({status, data}) => {
-                        console.log({status, data})
-                        res.status(status).send({message: `Plugin ${pluginName} failed to register DOI: ${input.doi}`, data})
-                    })
-            })
-
+                  .then(data => resolve(data))
+                  .catch(reject)
+              })
+            }))
+              .then(registered => {res.status(201).send(registered)})
+              .catch(({status, data}) => res.status(status).send(data))
+          })
         })
     })
 
