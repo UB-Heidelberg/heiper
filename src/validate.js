5b434866f611971d1ca1100e17f274116d9a1c23
Nils Weiher <weiher@ub.uni-heidelberg.de>
2021-01-13 10:07:41 +0100

    Updated all dependencies to current versions.
    Migrate to package ajv version 7.0.3, added ajv-formats package because of this.
    Fixed require and added ajv-formats plugin initialisation.

--- src/validate.js
+++ src/validate.js
@@ -1,8 +1,13 @@
-const Ajv = require('ajv')
+const Ajv = require('ajv').default
+// Required vor v7 of ajv.
+// See https://ajv.js.org/docs/validation.html#formats
+// and https://github.com/ajv-validator/ajv/issues/1295
+const addFormats = require('ajv-formats').default
 const ajv = new Ajv({
     allErrors: true,
     useDefaults: true, // This will expand defaults defined in the schema in the data
 })
+addFormats(ajv)
 const traf = new(require('traf'))()
 
 const schemaJSON = traf.parseFileSync(`${__dirname}/../data/schema.yml`)
